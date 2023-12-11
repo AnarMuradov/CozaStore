df6c85f89b6640ddabc4c716a37d8029bb3a7184
AnarMuradov <mi82nr231@code.edu.az>
2023-12-12 01:53:25 +0400

    Bag fix

--- src/components/HomeComponents/Products/index.jsx
+++ src/components/HomeComponents/Products/index.jsx
@@ -1,41 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import './style.scss'
+import React, { useEffect, useState } from "react";
+import "./style.scss";
 const Products = () => {
-    const [api, setApi] = useState([])
-   function getapi() {
-    fetch("https://6573ac96f941bda3f2af125e.mockapi.io/juan-store/api/v1/products")
-    .then(res=>res.json())
-    .then(data=>setApi(data))
-   }
-   useEffect(() => {
-    getapi()
-   }, [])
-   
+  const [api, setApi] = useState([]);
+  function getapi() {
+    fetch(
+      "https://6573ac96f941bda3f2af125e.mockapi.io/juan-store/api/v1/products"
+    )
+      .then((res) => res.json())
+      .then((data) => setApi(data));
+  }
+  useEffect(() => {
+    getapi();
+  }, []);
 
   return (
-    <section id='Products'>
-        <div className='productsSection'>
-            <div className='ProductsContainer'>
-                {api.map((x)=>{
-                    return(
-                        <div className='product' key={x.id}>
-                        <img className="productsImg" src={x.images} alt="" />
-                        <div className='prodName'>
-                        <h4>{x.model}</h4>
-                        <i class="fa-regular fa-heart"></i>
-                        </div>
-                        <p>${x.price }</p>
-                        <div className='prodIcon'>
-                        
-                        </div>
-                        </div>
-                    )
-                })}
-            </div>
-
+    <section id="Products">
+      <div className="productsSection">
+        <div className="ProductsContainer">
+          {api.map((x) => {
+            return (
+              <div className="product" key={x.id}>
+                <img className="productsImg" src={x.images} alt="" />
+                <div className="prodName">
+                  <h4>{x.model}</h4>
+                  <i class="fa-regular fa-heart"></i>
+                </div>
+                <p>${x.price}</p>
+                <div className="prodIcon"></div>
+              </div>
+            );
+          })}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
